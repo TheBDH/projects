54a095b0b72dd7825b7376eb082186420924402c
asing122 <annika_singh@brown.edu>
2025-08-17 13:37:52 -0700

    small fixes

--- 2025/08/17/brown-trump-agreement-reactions/script.js
+++ 2025/08/17/brown-trump-agreement-reactions/script.js
@@ -42,14 +42,6 @@ const messages = [
     message:
       "The deal between Brown and the Trump administration is undeniably a protection racket, and the price Brown paid has compromised the safety of Brown’s transgender community. Even beyond this, Brown’s capitulation makes the institution feel increasingly like an extension of the Trump administration itself, which directly undermines the University's mission while also creating a chilling effect on the kinds of research Brown’s staff, faculty and students can engage in.",
   },
-  //   {
-  //     name: "Alexander Green '99",
-  //     affiliation: "Alumni",
-  //     approval: "strongly approve",
-  //     pullQuote: "...the Trump administration has created a new normal.",
-  //     message:
-  //       "I thought the agreement was a good compromise that didn't give away too much or accept terms from the government which would have compromised the academic integrity of the University. It stinks that Brown was in this position in the first place but we have to accept, to some extent, that the Trump administration has created a new normal. ",
-  //   },
   {
     name: "Jane Lancaster, PhD'98, Adjunct faculty in History Department",
     affiliation: "Alumni",
@@ -59,15 +51,6 @@ const messages = [
     message:
       "I am concerned about Brown's efforts to pay for the settlement, including the potential sale/redevelopment of 20 Olive St., which was, for over a hundred years, the home of the Providence Shelter for Colored Children. Originally founded in 1838, the Shelter — which still exists as a fund to help local children of color — is one of the oldest continuous charities in Rhode Island. That history should not be ignored.",
   },
-  //   {
-  //     name: "Nicole Collins PhD 2030",
-  //     affiliation: "Student",
-  //     approval: "strongly disapprove",
-  //     pullQuote:
-  //       "You cannot negotiate with fascists; fascism will eat up everything in its path.",
-  //     message:
-  //       "I am a trans woman and a PhD student here, studying LGBTQ+ issues in the U.S. I am appalled by the deal with the government and am worried that I will personally be discriminated against — and that my research will be curtailed and possibly halted — due to Brown’s complicity with the Trump administration. I am one of many minorities Paxson has thrown away with this deal, and I will not remain quiet about this capitulation to fascism, even if it got us a momentary gain. You cannot negotiate with fascists; fascism will eat up everything in its path. It will only be a short time until Brown hands the names and contact information of dissidents over to the government.",
-  //   },
   {
     name: "Eileen Rudden '72",
     affiliation: "Alumni",
@@ -104,15 +87,6 @@ const messages = [
     message:
       "I am revolted by the deal Brown has struck with the Trump administration, and particularly its cowardly attacks on its own trans students, particularly trans women. The University should know its deal will do little to protect them from a hostile and capricious president.\nWhen I transitioned at Brown, I struggled with intense family rejection, the indignities of social and medical transition, isolation and rising political attacks on my community — on top of the normal stress of a rigorous college workload. Now, trans students will also have to worry about whether Trump’s executive orders apply to the bathrooms in their libraries, dormitories and lecture halls. There are already few trans women at the University, and Brown’s explicitly hostile concessions will further marginalize students who disproportionately experience discrimination and financial hardship.\nThe University played a critical role in the current anti-trans fervor by popularizing shoddy claims about “social contagion” in 2018. Now, the movement Brown helped to fuel is harming some of its most vulnerable students. I will not be donating to, or otherwise supporting the University, going forward.",
   },
-  //   {
-  //     name: "Nancy Lee",
-  //     affiliation: "Parent/guardian",
-  //     approval: "strongly approve",
-  //     pullQuote:
-  //       "Brown is handling this unprecedented situation and these difficult times as well as possible.",
-  //     message:
-  //       "Brown is handling this unprecedented situation and these difficult times as well as possible.",
-  //   },
   {
     name: "Kevin Seaman '69",
     affiliation: "Alumni",
@@ -131,15 +105,6 @@ const messages = [
     message:
       "I feel a lot of relief and gratitude that President Christina Paxson P’19 P’MD’20 was able to broker an agreement that, to a large extent, stayed true to Brown's mission and to our community. The one point troubling me is the adoption of the federal definitions of gender and the impact it has on our transgender and nonbinary community. This seems to validate the current administration’s weaponization of the issue, and I worry that it might embolden someone at Brown with discomfort around gender identity to take it upon themselves to police bathroom use. I hope we can work together to show our continued support for our transgender and nonbinary community and make it explicitly clear that any direct confrontation with others would be considered harassment and against Brown's policies.",
   },
-  //   {
-  //     name: "Donna Schmidt P'21",
-  //     affiliation: "Parent/guardian",
-  //     approval: "strongly disapprove",
-  //     pullQuote:
-  //       "I wish my daughter, who graduated in 2021, had chosen to attend a school with the integrity to stand up to oppression.",
-  //     message:
-  //       "I wish my daughter, who graduated in 2021, had chosen to attend a school with the integrity to stand up to oppression. Caving for money shows an utter lack of commitment to what I thought Brown stood for.",
-  //   },
   {
     name: "Francois Luks",
     affiliation: "Faculty",
@@ -200,17 +165,8 @@ const messages = [
     message:
       "I'm disappointed. If those who know better kneel, who will stand? They immediately went after other universities.",
   },
-  //   {
-  //     name: "Scott Whall",
-  //     affiliation: "Parent/guardian",
-  //     approval: "strongly disapprove",
-  //     pullQuote:
-  //       "The amount of kowtowing by colleges and universities at this time is truly upsetting.",
-  //     message:
-  //       "Frankly, I'm very disappointed. The amount of kowtowing by colleges and universities at this time is truly upsetting. This will only pave the way for his movement to fully control speech, academics and the free-thinking that made this nation great.  I was hoping for a stronger union against this oppression among the Ivy League members, but with half settling with this administration (that is SCARED of higher education), I think Paxson's acquiescence will only make this decision look much worse in the years to come.",
-  //   },
   {
-    name: "Ryan Spinney MPA'26",
+    name: "Ryan Spinney GS",
     affiliation: "Student",
     approval: "strongly disapprove",
     pullQuote:
@@ -236,14 +192,6 @@ const messages = [
     message:
       "I assume and believe that, despite its much weaker bargaining position relative to the federal administration, Brown struck the best deal possible. Brown appears to have given very little, if anything, and agreed to no changes it was not already making or ready to make. In doing so, it eliminated an existential risk, avoided substantial legal costs and removed a major distraction from its mission. That said, I am sorry that Brown was forced to settle this matter, rather than stand on principle, and am concerned that there may be no effective way to prevent the counterparties from making further demands or otherwise violating their obligations.",
   },
-  //   {
-  //     name: "Ronald Wilson",
-  //     affiliation: "Providence community",
-  //     approval: "neither approve nor disapprove",
-  //     pullQuote: "The agreement is the lesser of two evils...",
-  //     message:
-  //       "The agreement is the lesser of two evils, and avoids further significant harm to the University's academic and research objectives.",
-  //   },
   {
     name: "Benjamin Daniel '29",
     affiliation: "Student",
@@ -298,15 +246,6 @@ const messages = [
     message:
       "I identify as a moderate and disagree with some of the liberal policies other Brown students champion. I like to view both sides of every issue and approach it with nuance rather than submitting to peer pressure. However, there is one part of the agreement that I have a serious problem with, which is the University’s recognition of Executive Order 14168, an order that defines people by their sex assigned at birth, which in many cases is not just disrespectful but also disruptive to people who may not have fit that gender for years, if not decades. There are dozens of transgender and nonbinary students who are a vital part of the Brown community, and I believe it is wrong to sell out their wellbeing to appease some far right ideologues and think tanks. I don’t have a problem with some of the provisions but because this settlement contains a direct attack on innocent people who already face the hardship of being born into a gender to which they don’t belong, I must express serious disapproval with this settlement.",
   },
-  //   {
-  //     name: "Nathaniel Goodman '84",
-  //     affiliation: "Alumni",
-  //     approval: "somewhat approve",
-  //     pullQuote:
-  //       "...I think the end result is as good as could be hoped and seems largely cosmetic.",
-  //     message:
-  //       "While I'm disappointed that these policy adjustments have to come in conjunction with a supposed \"deal\" with this administration's bullying tactics, I think the end result is as good as could be hoped and seems largely cosmetic.",
-  //   },
   {
     name: "Lorraine Fryer '09",
     affiliation: "Alumni",
@@ -316,15 +255,6 @@ const messages = [
     message:
       'Disappointment. I understand that it was probably an increasingly fraught situation vis-a-vis funding and potentially having to let go of researchers. Nevertheless, I feel that Brown has relinquished important principles, and capitulating to bullies only leads to more bullying. Capitulating to facism only leads to more facism. If Brown launched a fundraising campaign as a response to funding being frozen, I would probably have donated more than I really can afford at this time. (I was laid off from my job of almost 8 years due to funding difficulties. I still say that we need to give a hard "not on my watch" to this administration.)',
   },
-  //   {
-  //     name: "Tim Fick",
-  //     affiliation: "Parent/guardian",
-  //     approval: "somewhat disapprove",
-  //     pullQuote:
-  //       "I was surprised that a deal was made and disappointed at the concessions given by Brown.",
-  //     message:
-  //       "I was surprised that a deal was made and disappointed at the concessions given by Brown. However, I understand the financial constraints Brown is facing and the need to arrive at an agreement with the government. Yet, what if the government demands more now that Brown conceded?",
-  //   },
   {
     name: "Jianye Hu P'28",
     affiliation: "Parent/guardian",
