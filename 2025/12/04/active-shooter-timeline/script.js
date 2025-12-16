c1a129012b0cfd105b521eb6dcf2b7062509bbdc
hadleybcarr <hadleybcarr@gmail.com>
2025-12-15 23:22:12 -0500

    original draft not working draft

--- 2025/12/04/active-shooter-timeline/script.js
+++ 2025/12/04/active-shooter-timeline/script.js
@@ -2,6 +2,7 @@ var timelineStep = d3.selectAll(".timeline-step");
 
 // var otherNewsStep = d3.selectAll(".other-news-step");
 
+/*CHANGE BACKGROUND IMAGES*/
 var images = [
   "./assets/background-photos/1.jpg",
   "./assets/background-photos/2.jpg",
@@ -9,19 +10,15 @@ var images = [
   "./assets/background-photos/4.jpg",
   "./assets/background-photos/5.jpg",
   "./assets/background-photos/6.jpg",
-  "./assets/background-photos/7.jpg",
-  "./assets/background-photos/8.jpg",
 ];
 
 var imageCredits = [
-  "Media by Ellis Rougeou | The Brown Daily Herald",
-  "Media by Stephanie London | The Brown Daily Herald",
   "Media by Kaia Yalamanchili | The Brown Daily Herald",
-  "Media by Sophia Leng | The Brown Daily Herald",
   "Media by Kaia Yalamanchili | The Brown Daily Herald",
-  "Media by Scout Chen | The Brown Daily Herald",
-  "Media by Ben Kang | The Brown Daily Herald",
-  "Media by Nat Hardy | The Brown Daily Herald",
+  "Media by Kaia Yalamanchili | The Brown Daily Herald",
+ "Media by Kaia Yalamanchili | The Brown Daily Herald",
+  "Media by Kaia Yalamanchili | The Brown Daily Herald",
+  "Media by Kaia Yalamanchili | The Brown Daily Herald",
 ];
 
 var sideImage = document.querySelector(".background");
@@ -44,9 +41,9 @@ scroller
       sideImageCaption.innerHTML = imageCredits[response.index - 2];
     } else if (response.index == 0 || response.index == 1) {
       timelineStep.classed("is-visible", false);
-      sideImageCaption.textContent =
+      /*sideImageCaption.textContent =
         "Media by Scout Chen | The Brown Daily Herald";
-      sideImage.style.backgroundImage = `url("./assets/background.jpg")`;
+      sideImage.style.backgroundImage = `url("./assets/background.jpg")`;*/
     }
 
     timelineStep
