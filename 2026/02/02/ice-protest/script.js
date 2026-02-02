a5f02d315ad7048562ad1d1332766b0692a15197
asing122 <annika_singh@brown.edu>
2026-02-01 23:20:38 -0500

    mobile changes to ice protest story

--- 2026/02/02/ice-protest/script.js
+++
@@ -1,4 +0,0 @@
-let vh = window.innerHeight * 0.01;
-let vw = window.innerWidth * 0.01;
-document.documentElement.style.setProperty("--vh", `${vh}px`);
-document.documentElement.style.setProperty("--vw", `${vw}px`);
